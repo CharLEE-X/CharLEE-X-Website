746598116dae72b8d3c1d16b88f1dbfe6c21195b
Adrian Witaszak <adrianwitaszak@gmail.com>
2023-07-02 10:00:26 +0100

    loader

--- docs/load.mjs
+++ docs/load.mjs
@@ -6,11 +6,11 @@ let te = null;
 try {
     await instantiate({ skia: Module['asm'] });
 } catch (e) {
-  te = e;  
+  te = e;
 }
 
 if (te == null) {
-    document.getElementById("warning").style.display="none";
+    document.getElementById("loading").style.display="none";
 } else {
     throw te;
 }
